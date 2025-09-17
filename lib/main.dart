91a1f959d818ac1159d481fcdfd7d33be28960b1
brandont118 <brandont118@gmail.com>
2025-09-17 12:42:44 -0700

    Added routes to 2nd screen and 3rd screen with the layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,70 +1,176 @@
 import 'package:flutter/material.dart';
 
+// void main() {
+//   runApp(const MyApp());
+// }
+
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MaterialApp(
+      title: 'Named Routes',
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const firstRoute(),
+        '/second': (context) => const secondRoute(),
+        '/third': (context) => const thirdRoute(),
+      },
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
 
-  // This widget is the root of your application.
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'offdabreak',
+//       theme: ThemeData(
+//         // This is the theme of your application.
+//         //
+//         // This works for code too, not just values: Most code changes can be
+//         // tested with just a hot reload.
+//       ),
+//       home: const MyHomePage(title: 'offdabreak'),
+//     );
+//   }
+// }
+
+// class MyHomePage extends StatefulWidget {
+//   const MyHomePage({super.key, required this.title});
+
+//   final String title;
+
+//   @override
+//   State<MyHomePage> createState() => _MyHomePageState();
+// }
+
+// class _MyHomePageState extends State<MyHomePage> {
+
+//   @override
+//   Widget build(BuildContext context) {
+//     // The Flutter framework has been optimized to make rerunning build methods
+//     // fast, so that you can just rebuild anything that needs updating rather
+//     // than having to individually change instances of widgets.
+//     return Scaffold(
+//       appBar: AppBar(
+//         // TRY THIS: Try changing the color here to a specific color (to
+//         // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+//         // change color while the other colors stay the same.
+//        backgroundColor: Color.fromARGB(255, 0, 225, 255),
+//         // Here we take the value from the MyHomePage object that was created by
+//         // the App.build method, and use it to set our appbar title.
+//         title: Text(widget.title),
+//       ),
+//       body: Center(
+//         child: Transform.rotate(
+//           angle: 1.5708, // 90 degrees in radians
+//           child: Image(
+//             image: NetworkImage('https://pbleagues.com/uploads/layouts/8927/1-NXL_2025-TEXAS_2d.jpg'),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+// ignore: camel_case_types
+class firstRoute extends StatelessWidget {
+  const firstRoute({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'offdabreak',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('offdabreak'),
+        backgroundColor: Color.fromARGB(255, 0, 225, 255),
       ),
-      home: const MyHomePage(title: 'offdabreak'),
+      body: ListView(
+        padding: const EdgeInsets.all(10),
+        children: [
+          Center(
+            child: ElevatedButton(
+              child: const Text('Second screen'),
+              onPressed: () {
+                Navigator.pushNamed(context, '/second');
+              },
+            ),
+          ),
+          Center(
+            child: ElevatedButton(
+              child: const Text('Third screen'),
+              onPressed: () {
+                Navigator.pushNamed(context, '/third');
+              },
+            ),
+          ),
+          Center(
+            child: ElevatedButton(
+              child: const Text('fourth screen'),
+              onPressed: () {
+                Navigator.pushNamed(context, '/third');
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+// ignore: camel_case_types
+class secondRoute extends StatelessWidget {
+  const secondRoute({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  // ignore: dead_code
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("GFG Second Route"),
+        backgroundColor: Color.fromARGB(255, 0, 225, 255),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text('Go back!'),
+        ), // ElevatedButton
+      ),
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+// ignore: camel_case_types
+class thirdRoute extends StatelessWidget {
+  const thirdRoute({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-       backgroundColor: Color.fromARGB(255, 0, 225, 255),
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text("GFG Third Route"),
+        backgroundColor: Color.fromARGB(255, 0, 225, 255),
       ),
-      body: Center(
-        child: Transform.rotate(
-          angle: 1.5708, // 90 degrees in radians
-          child: Image(
-            image: NetworkImage('https://pbleagues.com/uploads/layouts/8927/1-NXL_2025-TEXAS_2d.jpg'),
+      body: Column(
+        children: <Widget>[
+          Container(
+            child: Image(
+              image: NetworkImage(
+                'https://pbleagues.com/uploads/layouts/8927/1-NXL_2025-TEXAS_2d.jpg',
+              ),
+            ),
           ),
-        ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Text('Go back!'),
+          ),
+        ],
       ),
     );
   }
