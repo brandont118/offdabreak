47391754ef6434efacaa7ce9e97abd0e5fea0674
brandont118 <brandont118@gmail.com>
2025-11-05 11:47:37 -0800

    added point selecting page

--- lib/point_selectpg.dart
+++ lib/point_selectpg.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 // Here is were we would. display each match set of points played in the match
-// TODO: Set up a grid 2 wide
 // TODO: Add an Add point button that takes in the new data of ( Who wins, Where players went).
 class PointSelect extends StatefulWidget {
   const PointSelect({super.key});
@@ -21,34 +20,65 @@ class _PointSelectState extends State<PointSelect> {
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: GridView.builder(
-  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-    crossAxisCount: 2,
-    crossAxisSpacing: 10,
-    mainAxisSpacing: 10,
-  ),
-  itemCount: items.length,
-  itemBuilder: (context, index) {
-    return InkWell(
-      onTap: () {
-        print("Clicked tile $index");
-        // navigate or do something else
-        Navigator.pushNamed(context, '/fourth', arguments: items[index]);
-      },
-      child: Container( //TODO: Here is where the preview image would be as well as which point is which
-        decoration: BoxDecoration(
-          color: Colors.blue,
-          borderRadius: BorderRadius.circular(12),
-        ),
-        child: Center(
-          child: Text(
-            items[index],
-            style: TextStyle(color: Colors.white, fontSize: 18),
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
+            mainAxisExtent: 294,
           ),
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+            return InkWell(
+              onTap: () {
+                print("Clicked tile $index");
+                // navigate or do something else
+                Navigator.pushNamed(
+                  context,
+                  '/fourth',
+                  arguments: items[index],
+                );
+              },
+              //TODO: Here is where the preview image would be as well as which point is which
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Column(
+                  children: [
+                    Container(
+                      height: 20,
+                      width: double.maxFinite,
+                      color: Colors.red,
+                      child: Text(
+                        'Team A',
+                        textAlign: TextAlign.center,
+                        ),
+                    ),
+                    RotatedBox(
+                   quarterTurns: 1,
+                  child: Image.asset('assets/lonestar_layout.png'),
+                ),
+                  //   Text(
+                  //     items[index],
+                  //     style: TextStyle(color: Colors.white, fontSize: 18),
+                  // ),
+                  Spacer(),
+                  Container(
+                      height: 20,
+                      width: double.maxFinite,
+                      color: Colors.lightBlue,
+                      child: Text(
+                        'Team B',
+                        textAlign: TextAlign.center,
+                        ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
         ),
-      ),
-    );
-  },
-),
       ),
     );
   }
